5c03989ea4137fc041b5d390cb33e44ceefd0f93
ryanleung <leungryan10@gmail.com>
2024-05-09 14:32:29 -0700

    a

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,20 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.*;
 public class Game {
+    int score = 0;
+    JLabel s;
     public Game() {
-        JFrame frame = new JFrame();
 
+        JFrame frame = new JFrame();
         GamePanel gPanel = new GamePanel();
+        JPanel b = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        s = new JLabel(""+score);
+        b.add(s);
+        gPanel.add(b, BorderLayout.NORTH);
+
+
         frame.add(gPanel);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,6 +25,13 @@ public class Game {
         boolean[][] sand = new boolean[500][500];
         public GamePanel() {
             setBackground(Color.WHITE);
+            addMouseListener(this);
+            addMouseMotionListener(this);
+            for (int i = 0; i < sand.length; i++) {
+                for (int j = 0; j < 150; j++) {
+                    sand[i][j] = true;
+                }
+            }
         }
 
         // This is the method that draws everything
@@ -23,8 +39,11 @@ public class Game {
             super.paintComponent(g);
             for (int i = 0; i < sand.length; i++) {
                 for (int j = 0; j < sand[0].length; j++) {
-                    if (sand[i][j]) {
+                    if (!sand[i][j]) {
                         g.setColor(Color.YELLOW);
+                        g.drawRect(i, j, 1, 1);
+                    } else {
+                        g.setColor(Color.WHITE);
                         g.drawRect(i, j, 1, 1);
                     }
                 }
@@ -34,14 +53,23 @@ public class Game {
 
         public void mousePressed(MouseEvent e) {
             System.out.println("a");
-            for (int i = -12; i < 12; i++) {
-                for (int j = -12; j < 12; j++) {
-                    sand[e.getX() + i][e.getY() + j] = true;
+
+            for (int i = -12; i <= 12; i++) {
+                for (int j = -12; j <= 12; j++) {
+                    if (i * i + j * j <= 144) {
+                        int x = e.getX() + i;
+                        int y = e.getY() + j;
+                        if (x >= 0 && x < sand.length && y >= 0 && y < sand[0].length) {
+                            sand[x][y] = true;
+                            score++;
+                            s.setText(""+score);
+                        }
+                    }
                 }
             }
             repaint();
         }
-        
+
         public void mouseReleased(MouseEvent e) {
 
         }
