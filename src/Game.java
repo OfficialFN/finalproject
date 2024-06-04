0c96741b0b1c3e2e290783cdc2c2d41290c8ff86
ryanleung <leungryan10@gmail.com>
2024-06-03 23:48:28 -0700

    final

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Game extends JPanel {
+
+public class Game extends JPanel implements ActionListener{
+    double gamescore;
     int score = 0;
     int fps;
     JLabel s;
@@ -10,17 +12,27 @@ public class Game extends JPanel {
     boolean isHolding = false;
     int lastlocx;
     int lastlocy;
-
-
-    public Game() {
-
+    JButton pour;
+    boolean canerase = true;
+    boolean pouring = false;
+    int level;
+    JPanel res;
+    boolean changing;
+    boolean running = true;
+    public Game(int level) {
+        this.setLayout(new BorderLayout());
+        this.level= level;
         gPanel = new GamePanel();
         JPanel b = new JPanel(new FlowLayout(FlowLayout.LEFT));
         s = new JLabel("Score: "+score);
         fpslabel = new JLabel("FPS: " + fps);
+        pour = new JButton("Pour");
+        pour.addActionListener(this);
         b.add(s);
         b.add(fpslabel);
+        b.add(pour);
         gPanel.add(b, BorderLayout.NORTH);
+
 
         this.add(gPanel);
         this.setSize(500,500);
@@ -28,13 +40,19 @@ public class Game extends JPanel {
         Thread loopThread = new Thread(this::gameLoop);
         loopThread.start();
     }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Pour")) {
+            canerase = false;
+            pouring = true;
+        }
+    }
 
     public void gameLoop(){
         final int targetfps = 60;
         final long nsbetween = 1000000000/ targetfps;
         long looptime = System.nanoTime();
         long lastFpsTime = 0;
-        while (true) {
+        while (running) {
             long now = System.nanoTime();
             long timedif = now - looptime;
             looptime = now;
@@ -46,74 +64,246 @@ public class Game extends JPanel {
                 lastFpsTime = 0;
                 fps = 0;
             }
-
-            gPanel.render();
-
-            try {
-                Thread.sleep( (looptime-System.nanoTime() + nsbetween)/1000000 );
-            } catch(Exception e) {
-                e.printStackTrace();
+            if (pouring) {
+                gPanel.pour();
             }
+            if (!changing && pouring) {
+                results();
+                running = false;
+            }
+            if (running){
+                gPanel.render();
+
+
+                gPanel.clear();
+
+                try {
+                    Thread.sleep((looptime - System.nanoTime() + nsbetween) / 1000000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public void results() {
+        try {
+            res = new JPanel(new FlowLayout());
+            res.add(new JLabel("Score: " + gamescore));
+
+            int targetcount = gPanel.check();
+            System.out.println(targetcount);
+            JLabel goals = new JLabel();
+            switch (targetcount) {
+                case 0 -> goals.setText("Extra Targets: ❌❌❌");
+                case 1 -> goals.setText("Extra Targets: ✅❌❌");
+                case 2 -> goals.setText("Extra Targets: ✅✅❌");
+                case 3 -> goals.setText("Extra Targets: ✅✅✅");
+                default -> goals.setText("error");
+            }
+            res.add(goals);
+            JLabel t = new JLabel();
+            if (gPanel.checkgoal()) {
+                t.setText("Main Target: ✅");
+            } else {
+                t.setText("Main Target: ❌");
+            }
+            res.add(t);
+            this.remove(gPanel);
+            this.add(res,BorderLayout.CENTER);
+            this.revalidate();
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 
     class GamePanel extends JPanel implements MouseListener, MouseMotionListener{
         boolean[][] sand = new boolean[500][500];
+        boolean[][] water = new boolean[500][500];
+        boolean[][] goal = new boolean[500][500];
+
+        boolean[][] stone = new boolean[500][500];
         public GamePanel() {
+
+            setPreferredSize(new Dimension(500, 500));
             setBackground(Color.WHITE);
             addMouseListener(this);
             addMouseMotionListener(this);
             for (int i = 0; i < sand.length; i++) {
-                for (int j = 0; j < 150; j++) {
-                    sand[i][j] = true;
+                for (int j = 0; j < sand[0].length; j++) {
+                    if (j > 100) {
+                        sand[i][j] = true;
+                    } else {
+                        water[i][j] = true;
+                    }
+
+                }
+
+            }
+            if(level==3) {
+                for (int i = 0; i < 230; i++) {
+                    for (int j = 280; j < 300; j++) {
+                        stone[i][j] = true;
+                    }
+                }
+
+                for (int i = 250; i < 500; i++) {
+                    for (int j = 280; j < 300; j++) {
+                        stone[i][j] = true;
+                    }
+                }
+
+                for (int i = 380; i < 410; i++) {
+                    for (int j = 180; j < 280; j++) {
+                        stone[i][j] = true;
+                    }
+                }
+
+                for (int i = 30; i < 50; i++) {
+                    for (int j = 250; j < 270; j++) {
+                        goal[i][j] = true;
+                    }
+                }
+
+                for (int i = 450; i < 470; i++) {
+                    for (int j = 250; j < 270; j++) {
+                        goal[i][j] = true;
+                    }
+                }
+
+                for (int i = 440; i < 460; i++) {
+                    for (int j = 440; j < 460; j++) {
+                        goal[i][j] = true;
+                    }
+                }
+
+                for (int i = 50; i < 150; i++) {
+                    for (int j = 400; j < 480; j++) {
+                        goal[i][j] = true;
+                    }
                 }
             }
+        }
+        public int check() {
+            int targetcount = 0;
+            outer:
+            for (int i = 30; i < 50; i++) {
+                for (int j = 250; j < 270; j++) {
+                    if (water[i][j]) {
+                        targetcount++;
+                        break outer;
+                    }
+                }
+            }
+            outer2:
+            for (int i = 450; i < 470; i++) {
+                for (int j = 250; j < 270; j++) {
+                    if (water[i][j]) {
+                        targetcount++;
+                        break outer2;
+                    }
+                }
+            }
+            outer3:
+            for (int i = 440; i < 460; i++) {
+                for (int j = 440; j < 460; j++) {
+                    if (water[i][j]) {
+                        targetcount++;
+                        break outer3;
+                    }
+                }
+            }
+            return targetcount;
+        }
+        public boolean checkgoal() {
+            for (int i = 50; i < 150; i++) {
+                for (int j = 400; j < 480; j++) {
+                    if (water[i][j]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
 
 
         public void render() {
             repaint();
-            //System.out.println("repaint");
+        }
+        boolean[][] tempwater = new boolean[water.length][water[0].length];
+        public void clear() {
+            for (int i = 0; i < water.length; i++) {
+                for (int j = 0; j < water[0].length; j++) {
+                    tempwater[i][j] = false;
+                }
+            }
+        }
+
+        public void pour() {
+            changing = false;
+            for (int i = 0; i < water.length; i++) {
+                for (int j = 0; j < water[i].length; j++) {
+                    tempwater[i][j] = water[i][j];
+                }
+            }
+
+            for (int i = 0; i < sand.length; i++) {
+                for (int j = 0; j < sand[i].length; j++) {
+                    if (!sand[i][j] && !tempwater[i][j] && !stone[i][j]) {
+                        boolean nexttowater = (i > 0 && tempwater[i - 1][j]) || (i < sand.length - 1 && tempwater[i + 1][j]) || (j > 0 && tempwater[i][j - 1]) || (j < sand[i].length - 1 && tempwater[i][j + 1]);
+
+                        if (nexttowater) {
+                            changing = true;
+                            water[i][j] = true;
+                        }
+                    }
+                }
+            }
         }
         protected void paintComponent(Graphics g) {
-            System.out.println("a");
             super.paintComponent(g);
             for (int i = 0; i < sand.length; i++) {
                 for (int j = 0; j < sand[0].length; j++) {
-                    if (!sand[i][j]) {
+                    if (goal[i][j]) {
+                        g.setColor(Color.GREEN);
+                        g.drawRect(i,j,1,1);
+                    } else if (stone[i][j]) {
+                        g.setColor(Color.GRAY);
+                        g.drawRect(i,j,1,1);
+                    } else if (sand[i][j]) {
                         g.setColor(Color.YELLOW);
                         g.drawRect(i, j, 1, 1);
+                    } else if (water[i][j]) {
+                        g.setColor(Color.BLUE);
+                        g.drawRect(i,j,1,1);
                     } else {
                         g.setColor(Color.WHITE);
                         g.drawRect(i, j, 1, 1);
                     }
                 }
             }
-            score = 0;
-            for (int i = 0; i < sand.length; i++) {
-                for (int j = 150; j < sand[0].length; j++) {
-                    if (sand[i][j]) {
-                        score++;
-                        s.setText("Score: "+score);
-
-                    }
-                }
-            }
+            gamescore = 100 - score/2000.0;
+            s.setText("Score: "+gamescore);
 
         }
 
         public void mousePressed(MouseEvent e) {
-            isHolding = true;
-            lastlocx = e.getX();
-            lastlocy = e.getY();
+            if (canerase){
+                isHolding = true;
+                lastlocx = e.getX();
+                lastlocy = e.getY();
 
-            for (int i = -12; i <= 12; i++) {
-                for (int j = -12; j <= 12; j++) {
-                    if (i * i + j * j <= 144) {
-                        int x = e.getX() + i;
-                        int y = e.getY() + j;
-                        if (x >= 0 && x < sand.length && y >= 0 && y < sand[0].length) {
-                            sand[x][y] = true;
+                for (int i = -12; i <= 12; i++) {
+                    for (int j = -12; j <= 12; j++) {
+                        if (i * i + j * j <= 144) {
+                            int x = e.getX() + i;
+                            int y = e.getY() + j;
+                            if (x >= 0 && x < sand.length && y >= 0 && y < sand[0].length) {
+                                if (sand[x][y]) {
+                                    sand[x][y] = false;
+                                    score++;
+                                }
+
+                            }
                         }
                     }
                 }
@@ -133,18 +323,19 @@ public class Game extends JPanel {
         }
 
         public void mouseDragged(MouseEvent e) {
-            int x = e.getX();
-            int y = e.getY();
-            int step = 15;
-            int stepX = (x - lastlocx) / step;
-            int stepY = (y - lastlocy) / step;
-            for (int a = 0; a < step; a++) {
-                int newX = lastlocx + stepX * a;
-                int newY = lastlocy + stepY * a;
-                System.out.println(newX + " " + newY);
-                connect(newX,newY);
+            if (canerase){
+                int x = e.getX();
+                int y = e.getY();
+                int step = 15;
+                int stepX = (x - lastlocx) / step;
+                int stepY = (y - lastlocy) / step;
+                for (int a = 0; a < step; a++) {
+                    int newX = lastlocx + stepX * a;
+                    int newY = lastlocy + stepY * a;
+                    connect(newX, newY);
+                }
+                mousePressed(e);
             }
-            mousePressed(e);
         }
 
         public void mouseMoved(MouseEvent e) {
@@ -159,8 +350,12 @@ public class Game extends JPanel {
             for (int i = -12; i <= 12; i++) {
                 for (int j = -12; j <= 12; j++) {
                     if (i * i + j * j <= 144) {
-                        if (x >= 0 && x < sand.length && y >= 0 && y < sand[0].length) {
-                            sand[x+i][y+j] = true;
+                        if (x+i >= 0 && x+i < sand.length && y+j >= 0 && y+j < sand[0].length) {
+                            if (sand[x+i][y+j]) {
+                                sand[x+i][y+j] = false;
+                                score++;
+                            }
+
                         }
 
                     }
--- src/Home.java
+++ src/Home.java
@@ -7,19 +7,52 @@ public class Home implements ActionListener{
     JFrame frame;
     JPanel home;
     JButton playbutton;
-    JButton howto;
+    JButton l1,l2,l3;
+    int level=0;
+    JPanel name, levels, p, h, a,b,c;
     public Home () {
         frame = new JFrame();
         home = new JPanel();
-        home.setLayout(new FlowLayout());
+        home.setLayout(new BoxLayout(home, BoxLayout.Y_AXIS));
+
+        name = new JPanel(new FlowLayout());
+        name.setSize(new Dimension(1000, 50));
+        JLabel n = new JLabel("Where's My Water? (only level 3 works)");
+        name.add(n);
+        home.add(name);
+
+        levels = new JPanel(new FlowLayout());
+        levels.setPreferredSize(new Dimension(1000, 50));
+        l1 = new JButton("Level 1");
+        l1.setPreferredSize(new Dimension(100,50));
+        l1.addActionListener(this);
+        levels.add(l1);
+
+        l2 = new JButton("Level 2");
+        l2.setPreferredSize(new Dimension(100,50));
+        l2.addActionListener(this);
+        levels.add(l2);
+
+        l3 = new JButton("Level 3");
+        l3.setPreferredSize(new Dimension(100,50));
+        l3.addActionListener(this);
+        levels.add(l3);
+        home.add(levels);
+
+        p = new JPanel(new FlowLayout());
         playbutton = new JButton("Play");
         playbutton.setPreferredSize(new Dimension(100,50));
         playbutton.addActionListener(this);
-        home.add(playbutton);
+        p.add(playbutton);
+        home.add(p);
 
-        howto = new JButton("How to play");
-        howto.setPreferredSize(new Dimension(100,50));
-        home.add(howto);
+
+        a = new JPanel();
+        home.add(a);
+        b = new JPanel();
+        home.add(b);
+        c = new JPanel();
+        home.add(c);
 
         frame.add(home);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,8 +61,33 @@ public class Home implements ActionListener{
 
 
     }
+    public void changeBack(Color d) {
+        name.setBackground(d);
+        levels.setBackground(d);
+        p.setBackground(d);
+        a.setBackground(d);
+        b.setBackground(d);
+        c.setBackground(d);
+
+
+    }
     public void actionPerformed(ActionEvent e) {
-        frame.remove(home);
-        frame.add(new Game());
+        if (e.getActionCommand().equals("Level 1")) {
+            changeBack(Color.GREEN);
+            level =1;
+        } else if (e.getActionCommand().equals("Level 2")) {
+            changeBack(Color.YELLOW);
+            level=2;
+        } else if (e.getActionCommand().equals("Level 3")) {
+            changeBack(Color.RED);
+            level=3;
+        } else if (e.getActionCommand().equals("Play")) {
+            if (level !=0) {
+                frame.remove(home);
+                frame.add(new Game(level));
+            }
+
+        }
+
     }
 }
